ac65fd2df19844222fd6065cdf9895f0441348f7
Liraz Siri <liraz@sterilesecurity.com>
2011-07-01 04:03:26 +0300

    test locking example

---
+++ tests/mp2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import time
+from multiprocessing import Process, Lock
+
+if __name__ == '__main__':
+    l = Lock()
+
+    def f(i, locking=True):
+        if locking:
+            l.acquire()
+        s = 'hello world ' + str(i)
+        for i in range(len(s)):
+            print s[i],
+            sys.stdout.flush()
+            time.sleep(0.00001)
+        print
+
+        if locking:
+            l.release()
+
+    procs = []
+    print "WITHOUT LOCKING"
+    for num in range(10):
+        p = Process(target=f, args=(num,False))
+        p.start()
+        procs.append(p)
+
+    for p in procs:
+        p.join()
+
+    print "WITH LOCKING"
+    for num in range(10):
+        Process(target=f, args=(num,True)).start()
