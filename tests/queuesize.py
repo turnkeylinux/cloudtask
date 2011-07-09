eac8b49b111608c1b498c4206a64ad6ef67b2b90
Liraz Siri <liraz@sterilesecurity.com>
2011-07-09 20:58:10 +0300

    add wait to my test Queue

--- tests/queuesize.py
+++ tests/queuesize.py
@@ -1,28 +1,63 @@
 #!/usr/bin/python
-from multiprocessing import Semaphore
+from multiprocessing import Process, Semaphore, Condition
 from multiprocessing.queues import Queue
 
-class SemQueue(Queue):
+import time
+
+class BetterQueue(Queue):
     """Queue that uses a semaphore to reliably count items in it"""
     def __init__(self, maxsize=0):
-        self.sem = Semaphore(0)
+        self.sem_items = Semaphore(0)
+        self.cond_empty = Condition()
+
         Queue.__init__(self, maxsize)
+
+    def __getstate__(self):
+        return Queue.__getstate__(self) + (self.sem_items, self.cond_empty)
+
+    def __setstate__(self, state):
+        Queue.__setstate__(self, state[:-2])
+        self.sem_items, self.cond_empty = state[-2:]
 
     def put(self, obj, block=True, timeout=None):
         Queue.put(self, obj, block, timeout)
-        self.sem.release()
+        self.sem_items.release()
 
     def get(self, block=True, timeout=None):
         ret = Queue.get(self, block, timeout)
-        self.sem.acquire()
+        self.sem_items.acquire()
+        if self.sem_items.get_value() == 0:
+            self.cond_empty.acquire()
+            try:
+                self.cond_empty.notify_all()
+            finally:
+                self.cond_empty.release()
+
         return ret
 
     def qsize(self):
-        return self.sem.get_value()
+        return self.sem_items.get_value()
+
+    def wait(self):
+        """Wait for all items to be read"""
+        self.cond_empty.acquire()
+        try:
+            if self.qsize():
+                self.cond_empty.wait()
+        finally:
+            self.cond_empty.release()
+
+def worker(input):
+    for sleep in iter(input.get, 'STOP'):
+        print "sleeping for %d seconds" % sleep
+        time.sleep(sleep)
 
 def test():
-    q = SemQueue()
+    bq = BetterQueue()
     for i in range(3):
-        q.put(i)
+        bq.put(3)
 
-    return q
+    p = Process(target=worker, args=(bq,))
+    p.start()
+
+    return p, bq
