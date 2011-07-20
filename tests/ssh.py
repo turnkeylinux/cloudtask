d4ddd14ec062de39faf50f679ea58fa9bed50d4f
Liraz Siri <liraz@turnkeylinux.org>
2011-07-20 03:40:13 +0300

    improved test case for SSHCommand

--- tests/ssh.py
+++ tests/ssh.py
@@ -2,11 +2,21 @@
 import sys
 from command import Command
 
+class SSHCommand(Command):
+    def __init__(self, address, command):
+        opts = ('StrictHostKeyChecking=no',
+                'PasswordAuthentication=no')
+
+        argv = ['ssh']
+        for opt in opts:
+            argv += [ "-o", opt ]
+
+        argv += [ address, command ]
+        Command.__init__(self, argv, setpgrp=True)
+
 command = 'echo HOME=$HOME; echo pid=$$; (while true; do sleep 1; echo hello; done)'
-args = ('ssh',
-        'd',
-        command)
-c = Command(args, setpgrp=True)
+
+c = SSHCommand('d', command)
 def cb(c, buf):
     if buf:
         sys.stdout.write(buf)
