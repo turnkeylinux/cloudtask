6c3ebad398ea4a84fac66090928dcf7e9f60856a
Liraz Siri <liraz@turnkeylinux.org>
2011-07-25 01:01:35 +0300

    fully implemented cli parsing

--- cloudtask_launch_workers.py
+++ cloudtask_launch_workers.py
@@ -8,14 +8,26 @@ it by hand.
 
 Options:
 
-    --apikey            Hub APIKEY
-                        Environment: HUB_APIKEY
+    --apikey      Hub APIKEY
+                  Environment: HUB_APIKEY
 
-    --ec2-region        Region for instance launch (default: us-east-1)
-    --ec2-size          Instance size (default: m1.small)
-    --ec2-type          Instance type <s3|ebs> (default: s3)
+    --region      Region for instance launch (default: us-east-1)
+                  Regions:
 
-    --label             Optional server description
+                    us-east-1 (Virginia, USA)
+                    us-west-1 (California, USA)
+                    eu-west-1 (Ireland, Europe)
+                    ap-southeast-1 (Singapore, Asia)
+
+    --size        Instance size (default: m1.small)
+                  Sizes:
+
+                    t1.micro (1 CPU core, 613M RAM, no tmp storage)
+                    m1.small (1 CPU core, 1.7G RAM, 160G tmp storage)
+                    c1.medium (2 CPU cores, 1.7G RAM, 350G tmp storage)
+
+    --type        Instance type <s3|ebs> (default: s3)
+    --label       Hub description label for all launched servers
 
 Usage example:
 
@@ -23,6 +35,7 @@ Usage example:
 
 """
 
+import os
 import sys
 import getopt
 
@@ -36,15 +49,25 @@ def usage(e=None):
     print >> sys.stderr, __doc__.strip()
     sys.exit(1)
 
+def fatal(e):
+    print >> sys.stderr, "error: " + str(e)
+    sys.exit(1)
+
 def main():
+    kwargs = {
+        'region': "us-east-1",
+        'size': "m1.small",
+        'type': "s3",
+        'label': "",
+    }
+
+    opt_apikey = os.environ.get('HUB_APIKEY', os.environ.get('CLOUDTASK_APIKEY'))
+    
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 
-                                       'h', [ 'help',
-                                              'apikey=',
-                                              'ec2-region=',
-                                              'ec2-size=',
-                                              'ec2-type=',
-                                              'label=' ])
+        opts, args = getopt.getopt(sys.argv[1:], 
+                                   'h', [ 'help',
+                                          'apikey=' ] + 
+                                        [ key + '=' for key in kwargs ])
     except getopt.GetoptError, e:
         usage(e)
 
@@ -52,8 +75,24 @@ def main():
         if opt in ('-h', '--help'):
             usage()
 
+        if opt == '--apikey':
+            opt_apikey = val
+
+        for key in kwargs:
+            if opt == '--' + key:
+                kwargs[key] = val
+                break
+
+    if not opt_apikey:
+        fatal("missing required APIKEY")
+
     if len(args) < 2:
         usage()
+
+    howmany = args[1]
+
+    print `opt_apikey`
+    print `kwargs`
 
 if __name__ == "__main__":
     main()
