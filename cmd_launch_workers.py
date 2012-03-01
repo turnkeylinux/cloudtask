3a066dd06c8b27c29acd106d233e7f865c2b52a9
Liraz Siri <liraz@turnkeylinux.org>
2012-03-02 01:39:58 +0200

    added --install-updates option to launch-workers command

--- cmd_launch_workers.py
+++ cmd_launch_workers.py
@@ -19,28 +19,30 @@ it by hand.
 
 Options:
 
-    --hub-apikey   Hub APIKEY
-                   Environment: HUB_APIKEY
+    --hub-apikey       Hub APIKEY
+                       Environment: HUB_APIKEY
 
-    --backup-id    TurnKey Backup ID to restore
+    --backup-id        TurnKey Backup ID to restore
 
-    --region       Region for instance launch (default: us-east-1)
-                   Regions:
+    --region           Region for instance launch (default: us-east-1)
+                       Regions:
 
-                     us-east-1 (Virginia, USA)
-                     us-west-1 (California, USA)
-                     eu-west-1 (Ireland, Europe)
-                     ap-southeast-1 (Singapore, Asia)
+                         us-east-1 (Virginia, USA)
+                         us-west-1 (California, USA)
+                         eu-west-1 (Ireland, Europe)
+                         ap-southeast-1 (Singapore, Asia)
 
-    --size         Instance size (default: m1.small)
-                   Sizes:
+    --size             Instance size (default: m1.small)
+                       Sizes:
 
-                     t1.micro (1 CPU core, 613M RAM, no tmp storage)
-                     m1.small (1 CPU core, 1.7G RAM, 160G tmp storage)
-                     c1.medium (2 CPU cores, 1.7G RAM, 350G tmp storage)
+                         t1.micro (1 CPU core, 613M RAM, no tmp storage)
+                         m1.small (1 CPU core, 1.7G RAM, 160G tmp storage)
+                         c1.medium (2 CPU cores, 1.7G RAM, 350G tmp storage)
 
-    --type         Instance type <s3|ebs> (default: s3)
-    --label        Hub description label for all launched servers
+    --type             Instance type <s3|ebs> (default: s3)
+    --label            Hub description label for all launched servers
+
+    --install-updates  Install security updates (by default they're skipped)
 
 Usage examples:
 
@@ -87,6 +89,7 @@ def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], 
                                        'h', [ 'help',
+                                              'install-updates',
                                               'hub-apikey=' ] + 
                                             [ key.replace('_', '-') + '=' 
                                               for key in kwargs ])
@@ -99,6 +102,9 @@ def main():
 
         if opt == '--hub-apikey':
             hub_apikey = val
+
+        if opt == '--install-updates':
+            kwargs['sec_updates'] = 'INSTALL'
 
         for key in kwargs:
             if opt == '--' + key.replace('_', '-'):
